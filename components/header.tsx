43f5ce15fd97f5252f8fa362a712b72557c506d4
James Gomez <olagomez@live.co.uk>
2021-09-06 14:35:34 +0000

    change

---
+++ components/header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header: React.FC = ({ children }) => {
+  return (
+    <header className="h-16 px-8 bg-blue-600 text-white w-full text-center sticky top-0 flex items-center justify-center">
+      <span>HEADER</span>
+
+      <div className="h-10 w-10 bg-white rounded-full flex items-center justify-center text-blue-600 ">
+        <span className="material-icons items-center">face</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

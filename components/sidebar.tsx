8bcc297d29aa5f3f785bacbf06fd17daeda96221
olajameze <olagomez@live.co.uk>
2021-09-13 17:57:53 +0100

    change

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -26,18 +26,25 @@ const Navbar = () => {
 
 
 
-            <div className={sidebar ? 'active transform translate-x-0' : 'transform -translate-x-40'}>
-                <div className="bg-purple-600 w-60 max-h-screen flex flex-col">
-                    <button>
-                        <FaIcons.FaBars onClick={showSidebar} />
+            <div className={sidebar ? 'active transform translate-x-0' : 'transform -translate-x-0'}>
+                <div className="flex  space-x=8">
+                    <div className="bg-purple-600 w-60 max-h-screen flex flex-col">
+                        <div className="flex">
+                            <div>
+                                Navigation
+                            </div>
+                            <button>
+                                <FaIcons.FaBars onClick={showSidebar} />
+                            </button>
+                        </div>
 
-                    </button>
-                    <ul>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#products">Products</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact Us</a></li>
-                    </ul>
+                        <ul>
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#products">Products</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#contact">Contact Us</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
